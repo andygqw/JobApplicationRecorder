9d26ed0621587aa5335094b2502c572fcc1e0f45
andygqw <andygqwang@gmail.com>
2024-03-01 23:03:05 -0800

    Added session timeout

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
+from datetime import timedelta
 
 # Initialize the Flask application
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -10,6 +11,10 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "--s"
 app.config["MYSQL_DB"] = "JobApplications"
+
+# Set session to expire after 30 minutes of inactivity
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=15)
+
 # Extra configs, optional:
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # This enables dictionary cursor
 
