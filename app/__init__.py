ffc644946068edc426750f0e351728ce821d56ef
andygqw <andygqwang@gmail.com>
2024-03-01 21:45:12 -0800

    added row click edit modal

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 # Required
 app.config['MYSQL_HOST'] = 'localhost'
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "--s"
+app.config["MYSQL_PASSWORD"] = "Andywang0704!"
 app.config["MYSQL_DB"] = "JobApplications"
 # Extra configs, optional:
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # This enables dictionary cursor
