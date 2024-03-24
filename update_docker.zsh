6e684905dd5db7ee0a99dfe2d2ba549b9535c0db
andygqw <andygqwang@gmail.com>
2024-03-23 23:42:04 -0700

    fixed shell script doesn't delete image issue

--- update_docker.zsh
+++ update_docker.zsh
@@ -20,15 +20,15 @@ fi
 # Step 3: Remove the Docker image if it exists
 if docker images | grep -q $IMAGE_NAME; then
     echo "Removing image $IMAGE_NAME..."
-    docker rmi $IMAGE_NAME:$TAG
+    docker rmi $IMAGE_NAME
 fi
 
 # Step 4: Build a new Docker image from Dockerfile
-echo "Building new image $IMAGE_NAME:$TAG..."
+echo "Building new image $IMAGE_NAME ..."
 docker build -t $IMAGE_NAME .
 
 # Step 5: Run a container from the new Docker image
-echo "Running new container $CONTAINER_NAME from image $IMAGE_NAME:$TAG..."
+echo "Running new container $CONTAINER_NAME from image $IMAGE_NAME: ..."
 docker run --name $CONTAINER_NAME -p $PORTNUMBER:$PORTNUMBER $IMAGE_NAME
 
 # End of script
