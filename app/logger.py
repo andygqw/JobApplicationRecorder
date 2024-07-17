344c4feb07305b56ca10c64e94b395ac5e6c10fa
andygqw <andygqwang@gmail.com>
2024-07-17 04:54:42 -0700

    fixed datetime log issue

--- app/logger.py
+++ app/logger.py
@@ -23,13 +23,13 @@ def log(operation, user, notes, category):
             # cur.execute("INSERT INTO `Log` (operation, date, user_id, notes) VALUES (%s, %s, %s, %s)", (operation, datetime.now(), user_id, notes))
             # mysql.connection.commit()
             # cur.close()
-            response = views.make_request_by_query(f"INSERT INTO `log` (operation, date, user_id, notes) VALUES (\'{operation}\', \'{datetime.now().strftime('%Y-%m-%d')}\', \'{id[0][0]}\', \'{notes}\')")
+            response = views.make_request_by_query(f"INSERT INTO `log` (operation, date, user_id, notes) VALUES (\'{operation}\', \'{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\', \'{id[0][0]}\', \'{notes}\')")
             if not response.ok:
                 raise Exception(response.text)
         else:
             # cur.execute("INSERT INTO `Log` (operation, date, notes) VALUES (%s, %s, %s)", (operation, datetime.now(), notes))
             # mysql.connection.commit()
             # cur.close()
-            response = views.make_request_by_query(f"INSERT INTO `log` (operation, date, notes) VALUES (\'{operation}\', \'{datetime.now()}\', \'{notes}\')")
+            response = views.make_request_by_query(f"INSERT INTO `log` (operation, date, notes) VALUES (\'{operation}\', \'{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\', \'{notes}\')")
             if not response.ok:
                 raise Exception(response.text)
